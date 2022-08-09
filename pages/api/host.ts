560f6b973e3a4b68e76c8e4bc22f7899ad2ab9fd
Douglas Gusson <douglasgusson@gmail.com>
2022-08-09 07:56:19 -0300

    ✨ Adição consulta por domínio

---
+++ pages/api/host.ts
@@ -0,0 +1,26 @@
+import { lookup, reverse } from "dns";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const domain = req.body.domain;
+
+  lookup(domain, (err, address) => {
+    if (err) {
+      res.status(500).json({
+        message: "Não foi possível encontrar o servidor",
+        detail: err.message,
+      });
+    } else {
+      reverse(address, (err, hostnames) => {
+        if (err) {
+          res.status(500).json({
+            message: "Não foi possível encontrar o servidor",
+            detail: err.message,
+          });
+        } else {
+          res.status(200).json({ domain, hostnames, address });
+        }
+      });
+    }
+  });
+}
